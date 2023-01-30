6807c563a39c2a532955744466906718a4d4423f
sangsq <sang93@qq.com>
2023-01-30 17:17:02 +0800

    添加项目文件。

---
+++ MuYu/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MuYu;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MuYu/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MuYu;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
