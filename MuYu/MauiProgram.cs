8e97ebae579824e226e15ced159eedebb3bf631a
sangsq <sang93@qq.com>
2023-01-31 23:44:43 +0800

    ui 调整

--- MuYu/MauiProgram.cs
+++ MuYu/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using MuYu.Data;
 
 namespace MuYu;
 
@@ -20,8 +19,6 @@ public static class MauiProgram
 		builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 		
-		builder.Services.AddSingleton<WeatherForecastService>();
-
 		return builder.Build();
 	}
 }
